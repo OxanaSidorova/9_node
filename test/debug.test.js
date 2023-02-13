3c88a4664227d5fa28584fd390cb1a4470214c2c
OxanaSidorova <106552165+OxanaSidorova@users.noreply.github.com>
2023-02-13 11:33:05 +0000

    added 9.3 new

--- test/debug.test.js
+++ test/debug.test.js
@@ -3,15 +3,26 @@ const calculateBonus = require('../debug')
 describe ("Tests for calculated bonus function",() =>  {
     test ("Test for correct conditions",()=> {
         expect (calculateBonus(20,30) ).toBe(50);
-        expect(calculateBonus(20,29)).toEqual(49)
-        expect(calculateBonus(20,39)).toEqual(50)
+        // expect(calculateBonus(20,29)).toEqual(49)
+        // expect(calculateBonus(20,39)).toEqual(50)
+       
+    // })
+    // test ("Test for uncorrect conditions",()=> {
+    //     expect (calculateBonus(20,30) ).toBeGreaterThanOrEqual(50);
+    //     expect(calculateBonus(20,29)).toEqual(50)
+    //     expect(calculateBonus(20,39)).toEqual(59)
        
     })
-    test ("Test for uncorrect conditions",()=> {
-        expect (calculateBonus(20,30) ).toBeGreaterThanOrEqual(50);
-        expect(calculateBonus(20,29)).toEqual(50)
-        expect(calculateBonus(20,39)).toEqual(59)
-       
+    test ("Test for result with decimal",()=> {
+        expect (calculateBonus(19.9,30.1) ).toBe(50);
     })
+    test ("Test for <=",()=> {
+        expect (calculateBonus(20,29) ).toBeLessThan(50);
+    })
+    test ("Test for >=",()=> {
+        expect (calculateBonus(30,29) ).toBe(50);
+    })
+        
+        
 
 })
